c002b2da8eb421150317d362f3b333adcfc49ba7
altf4arnold <arnold.dech@adct.be>
2024-02-23 22:45:07 +0100

    404 and 500

--- tldtest/views.py
+++ tldtest/views.py
@@ -6,12 +6,9 @@ class Index(TemplateView):
     template_name = 'home.html'
 
 
-"""
 def handler404(request):
     return render(request, '404.html', status=404)
 
 
 def handler500(request):
     return render(request, '500.html', status=500)
-
-"""
