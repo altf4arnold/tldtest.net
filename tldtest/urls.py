4d76891d1b76be103cc94d80a9f68d3d7d746526
altf4arnold <arnold.dech@adct.be>
2024-02-23 04:01:36 +0100

    preping views

--- tldtest/urls.py
+++ tldtest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import Index
 
 urlpatterns = [
+    path("", Index.as_view(), name="home"),
     path('admin/', admin.site.urls),
 ]
---
+++ tldtest/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+"""
+def handler404(request):
+    return render(request, '404.html', status=404)
+
+
+def handler500(request):
+    return render(request, '500.html', status=500)
+
+"""
