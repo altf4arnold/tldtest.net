62e2138d5cf76af1151c8ea74bfad694d4c63127
altf4arnold <arnold.dech@adct.be>
2024-02-25 02:34:52 +0100

    removing zonefile-parser lib (does not do all we need) and droping the IANA zonefile (too complicated to parse) to go with a list of TLD's of ICAN (easyer to parse and then test individually)

--- tldtester/sorter.py
+++ tldtester/sorter.py
@@ -3,54 +3,31 @@ This file is dumping the IANA root zone and sorting it in the database
 Link to IANA website : https://www.internic.net/domain/root.zone
 """
 import urllib.request
-import zonefile_parser
 import json
-from tldtester.models import zonecontent
+# from tldtester.models import zonecontent, TLD
+import dns.resolver
 
 
 def downloader():
     """
-    Downloads the data. Returns None if not working, Returns data if working
+    Downloads the data. Returns None if not working, returns a list of TLD's if working
     """
-    url = urllib.request.urlopen("https://www.internic.net/domain/root.zone")
+    url = urllib.request.urlopen("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
     if url.getcode() == 200:
         raw = url.read()
-        raw = raw.decode("utf-8")
+        raw = raw.decode("utf-8").splitlines()
+        # File has a timestamp as first line. This will take it out so we only keep the TLD's
+        raw.pop(0)
     else:
         raw = None
     return raw
 
 
-def sorter(rawdata):
-    """
-    This file removes the tabs and line breaks from rawdata
-    returns as a list with dictionary in it
-    :returns: a list of dictionaries
-    """
-    encodeddata = zonefile_parser.parse(rawdata)
-    properdata = []
-    for line in encodeddata:
-        properdata.append(dict(json.loads(str(line).replace("'", '"'))))
-    return properdata
-
-
-def dbwriter(data):
-    """
-    Writes everything in the Zone database
-    """
-    for line in data:
-        DB = zonecontent()
-        DB.rtype = line["rtype"]
-        DB.name = line["name"]
-        DB.rclass = line["rclass"]
-        DB.data = line["rdata"]
-        DB.ttl = int(line["ttl"])
-        DB.save()
-
-
 def main():
     try:
-        dbwriter(sorter(downloader()))
+        data = sorter(downloader())
+        dbwriter(data)
+        print(data)
     except Exception as e:
         print(e)
 
